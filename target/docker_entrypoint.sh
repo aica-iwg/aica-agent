35a3dca9a1c4b2bd553b23fa56d78274356d9f4f
Benjamin Blakely <bblakely@anl.gov>
2021-09-01 19:00:51 -0700

    Standardize peripheral services on alpine:3 tag

--- target/docker_entrypoint.sh
+++ target/docker_entrypoint.sh
@@ -6,7 +6,7 @@ iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 ipset create honeypot hash:ip hashsize 4096 timeout 300
 iptables-legacy -t nat -A PREROUTING -m set --match-set honeypot src -j DNAT --to-destination `dig +short honeypot`
 
-service nginx start
-service ssh start
+echo "root:targetrootpassword" | chpasswd
+/usr/sbin/sshd -ef /etc/ssh/sshd_config
 
-tail -f /dev/null
+nginx -g "daemon off;"
