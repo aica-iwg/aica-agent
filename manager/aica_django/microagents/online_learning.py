60369a2a86dc97183b9ed75d28495d15ec210f68
Benjamin Blakely (ANL) <bblakely@anl.gov>
2022-04-22 14:55:50 -0500

    Initial nmap scan and conversion to STIX2 capability. Not doing anything with it yet.

--- manager/aica_django/microagents/online_learning.py
+++ manager/aica_django/microagents/online_learning.py
@@ -11,10 +11,15 @@
 
 import fcntl
 import logging
+import nmap3
+import socket
+import stix2
 
 from connectors.AicaNeo4j import AicaNeo4j
 from celery.app import shared_task
 from celery.utils.log import get_task_logger
+from hashlib import sha1
+from stix2.utils import get_timestamp
 
 logger = get_task_logger(__name__)
 
@@ -26,16 +31,87 @@ def load():
 
 
 @shared_task(name="ma-online_learning-nmap-to-stix")
-def nmap_to_stix(nmap_target, **nmap_args):
-    with open("/var/lock/aica-nmap-scan", "rw") as lockfile:
+def network_scan(nmap_target, nmap_args="-O -Pn --osscan-limit --host-timeout=60"):
+    hasher = sha1()
+    hasher.update(nmap_target.encode("utf-8"))
+    host_hash = hasher.hexdigest()
+    with open(f"/var/lock/aica-nmap-scan-{host_hash}", "rw") as lockfile:
         try:
             fcntl.flock(lockfile, fcntl.LOCK_EX | fcntl.LOCK_NB)
         except OSError:
             logging.warning(
-                "Couldn't obtain lock to run nmap scan, existing scan in-progress?"
+                "Couldn't obtain lock to run nmap scan of target, "
+                "existing scan in-progress?"
             )
             return False
 
-    graph_conn = AicaNeo4j()  # noqa: F841 (Temp until used)
+    # --Scan target(s) and add to graph -- #
 
-    return None
+    nmap = nmap3.Nmap()
+    results = nmap.scan_top_ports(nmap_target, args=nmap_args)
+    graph = AicaNeo4j()  # noqa: F841 (temp until used)
+
+    my_hostname = socket.gethostname()
+    my_ipv4 = stix2.IPv4Address(
+        value=socket.gethostbyname(my_hostname),
+    )
+    now = get_timestamp()
+
+    # Not needed and make iteration below messy
+    del results["stats"]
+    del results["runtime"]
+
+    for host, data in results.items():
+        if results[host]["state"]["state"] != "up":
+            continue
+
+        mac_addr = None
+        refs = []
+        if results[host]["macaddress"]:
+            mac_addr = stix2.MACAddress(
+                value=results[host]["macaddress"]["addr"],
+                # vendor
+            )
+            refs.append(mac_addr)
+
+        ipv4_addr = stix2.IPv4Address(
+            value=host,
+            resolves_to_refs=[mac_addr] if mac_addr else None,
+        )
+        refs.append(ipv4_addr)
+
+        for hostname in results[host]["hostname"]:
+            domain_name = stix2.DomainName(
+                value=hostname["name"],
+                resolves_to_refs=[ipv4_addr],
+            )
+            refs.append(domain_name)
+
+        if len(results[host]["osmatch"]) > 1:
+            os = results[host]["osmatch"][0]
+            operating_system = stix2.Software(
+                name=os["name"],
+                cpe=os["cpe"],
+                vendor=os["osclass"]["vendor"],
+                version=os["osclass"]["osgen"],
+            )
+            refs.append(operating_system)
+
+        for port in results[host]["ports"]:
+            if port["state"] == "open":
+                # Best fit STIX2.1 has...
+                open_port = stix2.NetworkTraffic(
+                    type="network-traffic",
+                    protocols=[port["protocol"]],
+                    dst_port=port["portid"],
+                    src_ref=my_ipv4,
+                    dst_ref=ipv4_addr,
+                    is_active=False,
+                )
+
+                observation = stix2.ObservedData(  # noqa: F841 (temp until used)
+                    first_observed=now,
+                    last_observed=now,
+                    number_observed=1,
+                    object_refs=[*refs, open_port],
+                )
