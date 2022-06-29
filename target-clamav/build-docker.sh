ae91e872863c6a682772fc0c297919a4e932d8c6
nayr <ryan.syed1305@gmail.com>
2022-06-29 15:31:35 -0500

    Not sure why the previous clamav folder wasn't removed

--- target-clamav/build-docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# For local testing only - not necessary for the actual docker-compose
-docker rm -f aica-target-clam
-docker build -t aica-target-clam .
-docker run --name=aica-target-clam --rm -it --cap-add SYS_ADMIN aica-target-clam
