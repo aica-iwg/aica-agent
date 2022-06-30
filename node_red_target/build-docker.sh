fc11be5848481696f9c9bd64d6050a2f1e962262
nayr <ryan.syed1305@gmail.com>
2022-06-30 11:30:33 -0500

    Should have removed all of the node red stuff to clean up the branch

--- node_red_target/build-docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# For local testing only - not necessary for the actual docker-compose
-docker rm -f node-red-target
-docker build -t node-red-target .
-docker run --rm -e "NODE_RED_CREDENTIAL_SECRET=your_secret_goes_here" -e "FLOWS=flows.json" -p 1880:1880 -v `pwd`:/data --name node-red node-red-target
