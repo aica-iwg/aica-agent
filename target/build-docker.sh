f063a7cb6bb79986a45d9b5364fc43bdb94643c9
nayr <ryan.syed1305@gmail.com>
2022-07-13 21:18:07 -0500

    Final commit, praying I did everything :)

--- target/build-docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# For local testing only - not necessary for the actual docker-compose
-docker rm -f aica-target
-docker build -t aica-target .
-docker run --name=aica-target --rm -it --cap-add SYS_ADMIN aica-target
