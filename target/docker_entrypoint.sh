990b9f3ccd6ea7acf954eb00eea6f084ed865c0d
bblakely <bblakely@anl.gov>
2022-10-05 12:57:30 -0500

    Cleanup post-merge of ClamAV functionality to Target.

--- target/docker_entrypoint.sh
+++ target/docker_entrypoint.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
+# Start syslog
+syslog-ng
+
+# Set up iptables
 iptables -P FORWARD ACCEPT
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 ipset create honeypot hash:ip hashsize 4096 timeout 300
 iptables-legacy -t nat -A PREROUTING -m set --match-set honeypot src -j DNAT --to-destination `dig +short honeypot`
 
+# Set up SSH access
 echo "root:targetrootpassword" | chpasswd
-echo "appuser:appuser" | chpasswd
-# Maybe there's a better way to let the manager know the ip address other than a mounted volume?
-echo "$(hostname),$(hostname -i)" > /var/log/clamav/hostinfo.txt
-
 /usr/sbin/sshd -ef /etc/ssh/sshd_config
 
+# Set up ClamAV pieces
+echo "$(hostname),$(hostname -i)" > /var/log/clamav/hostinfo.txt
 supervisord -c /etc/supervisord.conf &
+
+# Start Nginx
 nginx -g "daemon off;"
