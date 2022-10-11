ac3769a91774b45197a4cdc67ed031cc154ae561
bblakely <bblakely@anl.gov>
2022-10-11 15:12:37 -0500

    Basic bootstrap-driven front page. Non very functional, but pushing to replace the current 500 error.

--- manager/apps/aica_manager/views.py
+++ manager/apps/aica_manager/views.py
@@ -1,25 +1,39 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+import json2table  # type: ignore
+
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
 from aica_django.aica_celery import app
-from apps.aica_manager.models import Host, NetworkTraffic
+from apps.aica_manager.models import Host, Alert
 
 
-def modules(request):
+def modules(request: HttpRequest) -> HttpResponse:
     html = "<h1>AICA Manager</h1>"
     tasks = list(sorted(name for name in app.tasks if not name.startswith("celery.")))
     html += f"{tasks}"
     return HttpResponse(html)
 
 
-def overview(request):
+def overview(request: HttpRequest) -> HttpResponse:
+    data = dict()
+
     host_list = Host.nodes.all()
-    html = "<h1>Hosts</h1>"
-    html += ", ".join([str(x) for x in host_list])
+    data["hosts"] = json2table.convert(
+        {"Hosts": host_list},
+        table_attributes={
+            "id": "host_table",
+            "class": "table table-dark table-striped table-hover table-responsive",
+        },
+    )
 
-    flow_list = NetworkTraffic.nodes.all()
-    html += "<h1>Network Flows</h1>"
-    html += ", ".join([str(x) for x in flow_list])
+    # Recent Alerts
+    alert_list = Alert.nodes.all()
+    data["alerts"] = json2table.convert(
+        {"Alerts": alert_list},
+        table_attributes={
+            "id": "alert_table",
+            "class": "table table-dark table-striped table-hover table-responsive",
+        },
+    )
 
-    return HttpResponse(html)
-
-    # return render(request, "index.html")
+    return render(request, "index.html", data)
