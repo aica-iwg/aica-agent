08d8daa466e22dca9fdc5d0d5f1f8bf1579c0d48
Benjamin Blakely <99373210+bblakely-anl@users.noreply.github.com>
2022-10-17 13:20:38 -0500

    Anti virus knowledge (#92)
    Add correct parsing for ClamAV signature categories. Drop VirusTotal since we don't actually have the MD5 (we could manually compute that, so potentially add back later).

--- manager/aica_django/microagents/collaboration.py
+++
@@ -1,9 +0,0 @@
-# This microagent is responsible for coordinating external inputs and outputs such
-# as communication with other agents, command and control, or human operators. As
-# such, the input tasks here will likely consist of either polling of shared
-# database tables, or tasks called by Django REST endpoints. Outputs are likely to
-# be called by the decision making engine microagent.
-
-from celery.utils.log import get_task_logger
-
-logger = get_task_logger(__name__)
