843155a98ab4e6df4d6eb6e680a76a0c4fb2c8b0
Benjamin Blakely <99373210+bblakely-anl@users.noreply.github.com>
2024-04-02 16:18:24 -0500

    Add DNP3 fingerprint to DNP3 captures (#245)
    * Add DNP3 fingerprint to DNP3 captures
    
    * Remove unneeded function
    
    * Typing/Linting

--- manager/aica_django/connectors/DNP3.py
+++ manager/aica_django/connectors/DNP3.py
@@ -8,28 +8,56 @@ Functions:
     send_dnp3_command: (Not yet implemented) Send a DNP3 command to the network
 """
 
+import argparse
+import functools
+import glob
+import hashlib
+import json
 import pyshark  # type: ignore
 
 from celery import current_app
 from celery.app import shared_task
 from celery.utils.log import get_task_logger
+from typing import Any
+
 
 logger = get_task_logger(__name__)
 
 
-@shared_task(name="replay_dnp3_pcap")
-def replay_pcap(pcap_file: str) -> None:
-    cap = pyshark.FileCapture(pcap_file, display_filter="dnp3")
+# Thanks: https://stackoverflow.com/a/31174427
+def rgetattr(obj: object, attr: str) -> object:
+    def _getattr(obj: object, attr: object) -> object:
+        return getattr(obj, str(attr))
 
-    for packet in cap:
-        packet_dict = parse_dnp3_packet(packet)
-        if "al_func" not in packet_dict:
-            logger.warning("Ignoring unknown DNP3 packet type")
-        else:
-            current_app.send_task(
-                "ma-knowledge_base-record_dnp3",
-                [packet_dict],
-            )
+    return functools.reduce(_getattr, [obj] + attr.split("."))
+
+
+def object_try_get(
+    source_obj: dict[str, Any],
+    destination_dict: dict[str, Any],
+    source_attr: str,
+    destination_key: str,
+) -> dict[str, Any]:
+    try:
+        destination_dict[destination_key] = rgetattr(source_obj, source_attr)
+    except AttributeError:
+        pass
+
+    return destination_dict
+
+
+@shared_task(name="replay_dnp3_pcap")
+def replay_pcap(pcap_file: str, send_task: bool = True) -> None:
+    with pyshark.FileCapture(pcap_file, display_filter="dnp3") as cap:
+        for packet in cap:
+            packet_dict = parse_dnp3_packet(packet)
+            if send_task:
+                current_app.send_task(
+                    "ma-knowledge_base-record_dnp3",
+                    [packet_dict],
+                )
+            else:
+                print(packet_dict)
 
 
 @shared_task(name="capture_dnp3")
@@ -38,34 +66,87 @@ def capture_dnp3(interface: str) -> None:
     cap.sniff(timeout=50)
     for packet in cap.sniff_continuously(packet_count=5):
         packet_dict = parse_dnp3_packet(packet)
-        if "al_func" not in packet_dict:
-            logger.warning("Ignoring unknown DNP3 packet type")
-        else:
-            current_app.send_task(
-                "ma-knowledge_base-record_dnp3",
-                [packet_dict],
-            )
+        current_app.send_task(
+            "ma-knowledge_base-record_dnp3",
+            [packet_dict],
+        )
 
 
 def parse_dnp3_packet(packet: pyshark.packet.packet.Packet) -> dict[str, str]:
-    # See also: https://www.wireshark.org/docs/dfref/d/dnp3.html
-    log_dict = {
-        k: str(getattr(packet.dnp3, k))
-        for k in dir(packet.dnp3)
-        if k.startswith("al_iin_")
-        or k.startswith("al_objq_")
-        or k in ["src", "dst", "ctl_dir", "ctl_prm", "ctl_prifunc", "al_func", "al_uns"]
-    }
+    # See also:
+    # https://www.wireshark.org/docs/dfref/d/dnp3.html
+    # https://cdn.chipkin.com/assets/uploads/imports/resources/DNP3QuickReference.pdf
+    # https://research-repository.griffith.edu.au/bitstream/handle/10072/392608/Foo229955-Accepted.pdf?sequence=2
 
-    log_dict["sniff_timestamp"] = packet.sniff_timestamp
-    log_dict["src_host"] = packet.ip.src_host
-    log_dict["dst_host"] = packet.ip.dst_host
-    log_dict["srcport"] = packet.tcp.srcport
-    log_dict["dstport"] = packet.tcp.dstport
+    return_dict = dict()
 
-    return log_dict
+    # Non-DNP3 context
+    return_dict["sniff_timestamp"] = packet.sniff_timestamp
+    return_dict["ip_src"] = packet.ip.src_host
+    return_dict["ip_dst"] = packet.ip.dst_host
+    return_dict["tcp_src"] = packet.tcp.srcport
+    return_dict["tcp_dst"] = packet.tcp.dstport
+
+    # DNP3 Datalink Header
+    dnp3_dict = dict()
+    dnp3_dict["dnp3_datalink_from_master"] = packet.dnp3.ctl_dir
+    dnp3_dict["dnp3_datalink_from_primary"] = packet.dnp3.ctl_prm
+    if packet.dnp3.ctl_prm:
+        dnp3_dict["dnp3_datalink_function"] = packet.dnp3.ctl_prifunc
+    else:
+        dnp3_dict["dnp3_datalink_function"] = packet.dnp3.ctl_secfunc
+
+    dnp3_dict["dnp3_datalink_dst"] = (
+        packet.dnp3.dst
+    )  # Note: there may be multiple DNP3 dsts/srcs per IP!
+    dnp3_dict["dnp3_datalink_src"] = packet.dnp3.src
+
+    # DNP3 Application Header
+    dnp3_dict = object_try_get(
+        packet, dnp3_dict, "dnp3.al_uns", "dnp3_application_unsolicited_from_slave"
+    )
+    dnp3_dict = object_try_get(
+        packet, dnp3_dict, "dnp3.al_func", "dnp3_application_function"
+    )
+    dnp3_dict = object_try_get(packet, dnp3_dict, "dnp3.al_iin", "dnp3_application_iin")
+    dnp3_dict = object_try_get(packet, dnp3_dict, "dnp3.al_obj", "dnp3_application_obj")
+    dnp3_dict = object_try_get(
+        packet, dnp3_dict, "dnp3.al_objq_code", "dnp3_application_objq_code"
+    )
+    dnp3_dict = object_try_get(
+        packet, dnp3_dict, "dnp3.al_objq_index", "dnp3_application_objq_index"
+    )
+    dnp3_dict = object_try_get(
+        packet, dnp3_dict, "dnp3.al_objq_prefix", "dnp3_application_objq_prefix"
+    )
+    dnp3_dict = object_try_get(
+        packet, dnp3_dict, "dnp3.al_objq_range", "dnp3_application_objq_range"
+    )
+
+    # TODO: Determine the best way to incorporate object values into this without making the fingerprints too volatile
+
+    # Create a global identifier to represent the "type" of this message
+    fingerprint = hashlib.sha1(
+        json.dumps(dnp3_dict).encode("utf-8"), usedforsecurity=False
+    ).hexdigest()
+    return_dict["dnp3_fingerprint"] = fingerprint
+
+    return return_dict
 
 
 @shared_task(name="send_dnp3_command")
 def send_dnp3_command() -> None:
     raise NotImplementedError
+
+
+# For testing and inspection of DNP3 PCAPs
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-f", "--pcap-file", dest="pcap_file", nargs="+", type=str, required=True
+    )
+    args = parser.parse_args()
+
+    for pcap_file_expr in args.pcap_file:
+        for pcap_file in glob.glob(pcap_file_expr):
+            replay_pcap(pcap_file, send_task=False)
