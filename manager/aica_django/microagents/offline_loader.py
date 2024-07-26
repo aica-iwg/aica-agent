a9ef800d15f25a5b1a5464aee6fbaf2d7a1cd8d6
Benjamin Blakely <99373210+bblakely-anl@users.noreply.github.com>
2024-07-26 15:11:04 -0500

    Add sampling for DNP3 data (#294)

--- manager/aica_django/microagents/offline_loader.py
+++ manager/aica_django/microagents/offline_loader.py
@@ -139,12 +139,13 @@ def initialize(**kwargs: Dict[Any, Any]) -> None:
 
     # Replay DNP3 files if requested
     if getattr(settings, "REPLAY_PCAP", None):
-        pcap_files = list(glob.glob("pcaps/*/*/*.pcap"))
+        pcap_files = list(glob.glob("pcaps/*/DNP3 PCAP Files/*.pcap"))
         if len(pcap_files) == 0:
             logger.error("Requested to replay PCAPs, but none found.")
         else:
             for pcap_file in pcap_files:
                 logger.info(f"Replaying DNP3 PCAP file: {pcap_file}")
                 replay_dnp3_pcap.apply_async(
-                    kwargs={"pcap_file": pcap_file}, queue="pcap_replay"
+                    kwargs={"pcap_file": pcap_file, "sample": 0.1, "sample_min": 1000},
+                    queue="pcap_replay",
                 )
